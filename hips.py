c8dd6bffdcce1b9282f9c147590740ff8925d5fe
kernelland <kernelland@protonmail.ch>
2018-11-04 14:31:00 +0100

    Update hips.py

--- hips.py
+++ hips.py
@@ -3,14 +3,14 @@
 # H.I.P.S, by BufferBandit.
 # Hide your HTML source code by encoding it with tabs and spaces...
 
+
 import binascii
 
 class HIPS:
     
     def __init__(self,raw_html="<svg/onload=alert(1)>"):
         self.space,self.tab = " ","	"
-        self.raw_html = raw_html
-        
+        self.raw_html = raw_html      
 
     def text_to_bits(self,text, encoding='utf-8', errors='surrogatepass'):
         bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
@@ -22,11 +22,8 @@ class HIPS:
         return invisible_bits
 
     def create_html_file(self):
-        invisible_bits = self.get_encoded_bits(
-                                self.get_encoded_bits(
-                                self.raw_html )
-                                )
-        html_string = "<script>"+ "/"+invisible_bits +"""/.source.replace(/ /g, "0").replace(/	/g, "1").replace(/(\d{8})/g, '$1 ').replace(/(^\s+|\s+$)/,'').replace(/\d+./g,str=>String.fromCharCode('0b'+str)).replace(/.{7}/g,function(w){document.write(w)})""" +"</script>"
+        invisible_bits = self.get_encoded_bits(self.raw_html ) #document.write
+        html_string = "<script>"+ "/"+invisible_bits +"""/.source.replace(/ /g, "0").replace(/	/g, "1").replace(/(\d{8})/g, '$1 ').replace(/(^\s+|\s+$)/,'').replace(/\d+./g,str=>String.fromCharCode('0b'+str)).replace(/.{7}/g,function(w){alert(w)})""" +"</script>"
         return html_string
 
     def write_html(self,out_file="hips.html"):
