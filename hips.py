1e5d0c6cd277afcb317c73426e82ae05be37d895
kernelland <kernelland@protonmail.ch>
2018-11-04 14:13:49 +0100

    Update hips.py

--- hips.py
+++ hips.py
@@ -1,19 +1,39 @@
-# Things are hidden in plain sight...
+#/usr/bin/env python3
 
-space = " "
-tab = "	"
+# H.I.P.S, by BufferBandit.
+# Hide your HTML source code by encoding it with tabs and spaces...
 
 import binascii
 
-def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
-    bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
-    return bits.zfill(8 * ((len(bits) + 7) // 8))
+class HIPS:
+    
+    def __init__(self,raw_html="<svg/onload=alert(1)>"):
+        self.space,self.tab = " ","	"
+        self.raw_html = raw_html
+        
 
-i = input("HTML> ")
+    def text_to_bits(self,text, encoding='utf-8', errors='surrogatepass'):
+        bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
+        return bits.zfill(8 * ((len(bits) + 7) // 8))
 
-bits = text_to_bits(i)
+    def get_encoded_bits(self,raw_html):
+        bits = self.text_to_bits(self.raw_html)
+        invisible_bits = bits.replace("0",self.space).replace("1",self.tab)
+        return invisible_bits
 
-invisible = bits.replace("0",space).replace("1",tab)
+    def create_html_file(self):
+        invisible_bits = self.get_encoded_bits(
+                                self.get_encoded_bits(
+                                self.raw_html )
+                                )
+        html_string = "<script>"+ "/"+invisible_bits +"""/.source.replace(/ /g, "0").replace(/	/g, "1").replace(/(\d{8})/g, '$1 ').replace(/(^\s+|\s+$)/,'').replace(/\d+./g,str=>String.fromCharCode('0b'+str)).replace(/.{7}/g,function(w){document.write(w)})""" +"</script>"
+        return html_string
 
-with open("hips.html","w") as p:
-    p.write("<script>"+ "/"+invisible +"""/.source.replace(/ /g, "0").replace(/	/g, "1").replace(/(\d{8})/g, '$1 ').replace(/(^\s+|\s+$)/,'').replace(/\d+./g,str=>String.fromCharCode('0b'+str)).replace(/.{7}/g,function(w){document.write(w)})""" +"</script>")
+    def write_html(self,out_file="hips.html"):
+        with open(out_file,"w") as p:
+            p.write(self.create_html_file())
+            
+if __name__ == "__main__":
+    hips = HIPS()
+    hips.write_html()
+           
